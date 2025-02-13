0240fd8f62c8f48c32250e0daf20738e09c1a246
agnelnieves <agnelnieves@gmail.com>
2025-02-12 23:15:12 -0500

    move auth actions to its own file

---
+++ src/actions/auth.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { encodedRedirect } from "@/utils/utils";
+import { createClient } from "@/utils/supabase/server";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export const signInAction = async (formData: FormData) => {
+  const email = formData.get("email")?.toString();
+  const supabase = await createClient();
+  const origin = (await headers()).get("origin");
+
+  if (!email) {
+    return encodedRedirect("error", "/sign-in", "Email is required");
+  }
+
+  const { error } = await supabase.auth.signInWithOtp({
+    email,
+    options: {
+      emailRedirectTo: `${origin}/auth/callback`,
+    },
+  });
+
+  if (error) {
+    return encodedRedirect("error", "/sign-in", error.message);
+  }
+
+  return encodedRedirect(
+    "success",
+    "/sign-in",
+    "Check your email for the magic link to sign in!"
+  );
+};
+
+export const signOutAction = async () => {
+  const supabase = await createClient();
+  await supabase.auth.signOut();
+  return redirect("/sign-in");
+};
