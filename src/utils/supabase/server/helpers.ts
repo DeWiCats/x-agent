2266167b0ded8c6b851a77756ebb467b7e72602f
agnelnieves <agnelnieves@gmail.com>
2025-02-13 00:18:25 -0500

    add supabase user helpers

---
+++ src/utils/supabase/server/helpers.ts
@@ -0,0 +1,37 @@
+import { Database } from "@/types/database.types";
+import { createClient } from "../server";
+
+const supabase = await createClient();
+
+export async function getUserData(): Promise<
+  Database["public"]["Tables"]["users"]["Row"]
+> {
+  const { data: user } = await supabase.auth.getUser();
+
+  const { data: userData } = await supabase
+    .from("users")
+    .select("*")
+    .eq("id", user?.user?.id || "")
+    .single();
+
+  if (!userData) {
+    throw new Error("User not found");
+  }
+
+  return userData;
+}
+
+export async function updateUser(
+  user: Database["public"]["Tables"]["users"]["Row"]
+) {
+  const { data, error } = await supabase
+    .from("users")
+    .update(user)
+    .eq("id", user.id);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+}
