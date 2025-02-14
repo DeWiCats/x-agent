5107f8a30edbcdcffd38b3463328c2bbf0c59901
agnelnieves <agnelnieves@gmail.com>
2025-02-14 16:19:25 -0500

    change post date format and buttons

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -4,6 +4,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import Image from "next/image";
 import { imagePlaceholder } from "@/utils/utils";
 import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
 
 export default function PostCard({
   post,
@@ -25,16 +26,17 @@ export default function PostCard({
             <AvatarFallback>{agent.username}</AvatarFallback>
           </Avatar>
           <div className="flex-1 space-y-1">
-            <div className="flex items-center gap-2">
+            <div className="flex flex-col">
               <span className="font-semibold text-sline-text-dark-primary">
                 {agent.username}
               </span>
-              <span className="text-sline-text-dark-secondary hidden md:inline">
-                ·
-              </span>
               <span className="text-sline-text-dark-tertiary text-xs md:text-base">
                 {post.timestamp &&
-                  new Date(post.timestamp * 1000).toLocaleString()}
+                  new Date(post.timestamp * 1000).toLocaleDateString("en-US", {
+                    month: "short",
+                    day: "numeric",
+                    year: "numeric",
+                  })}
               </span>
             </div>
             {post.status && (
@@ -67,20 +69,26 @@ export default function PostCard({
         </div>
         <div className="mt-2 flex justify-between items-center gap-2">
           {post.x_url && (
-            <a
-              href={post.x_url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:opacity-80"
+            <Button
+              className="text-sline-text-dark-secondary hover:text-sline-text-dark-primary hover:bg-sline-alpha-dark-050 rounded-xl"
+              asChild
             >
-              <svg
-                viewBox="0 0 24 24"
-                className="w-5 h-5 text-sline-text-dark-secondary"
-                fill="currentColor"
+              <a
+                href={post.x_url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:opacity-80"
               >
-                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
-              </svg>
-            </a>
+                <svg
+                  viewBox="0 0 24 24"
+                  className="w-5 h-5 text-sline-text-dark-secondary"
+                  fill="currentColor"
+                >
+                  <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+                </svg>
+                Source
+              </a>
+            </Button>
           )}
           {post.score !== null && (
             <div
